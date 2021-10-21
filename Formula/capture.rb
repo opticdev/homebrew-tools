515412b1669161274f843dff91253378c9cb0f7e
GitHub Package Updater <action@github.com>
2021-10-21 15:04:48 +0200

    Bump to capture v0.2.1
    Signed-off-by: Ray Bejjani <ray.bejjani@useoptic.com>

--- Formula/capture.rb
+++ Formula/capture.rb
@@ -1,18 +1,18 @@
 # The platform/arch combinations that we support.
 $targets = {
  :macos_x86 => {
-   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.0/optic_capture-v0.2.0-x86_64-apple-darwin.tar.gz",
-   :sha => "ea4d43b793022f14e3daaaca2b1148e9cb409d27c268db521fcc4ea5e2a8f3a0",
+   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.1/optic_capture-v0.2.1-x86_64-apple-darwin.tar.gz",
+   :sha => "454629c853ec3c6c48bcc21b4b3d9000adcd671aa9e83f8e8c5b2c23c082fc01",
    :binary => "optic_capture-x86_64-apple-darwin" },
 
  :macos_arm => {
-   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.0/optic_capture-v0.2.0-aarch64-apple-darwin.tar.gz",
-   :sha => "6a39d9d27a86c336227b5b837b95835440edd2ccda8e710b4f4bba9d392b86b1",
+   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.1/optic_capture-v0.2.1-aarch64-apple-darwin.tar.gz",
+   :sha => "2ebe953b9aa6db539d5d2ae5a3adc55f89851ff62f916f93b787965acef2e463",
    :binary => "optic_capture-aarch64-apple-darwin" },
 
  :linux_x86 => {
-   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.0/optic_capture-v0.2.0-x86_64-unknown-linux-musl.tar.gz",
-   :sha => "ba736bb92f63c733e2884165237f4bfb639048adb551033dd812d038cab9863c",
+   :url => "https://s3.amazonaws.com/optic-packages/dists/optic_capture/v0.2.1/optic_capture-v0.2.1-x86_64-unknown-linux-musl.tar.gz",
+   :sha => "acbd0933e739ed4f444c5c796ebd357eac7160df13ebebeaac41fc53dfb25599",
    :binary => "optic_capture-x86_64-unknown-linux-musl" },
 }
 
@@ -43,6 +43,6 @@ class Capture < Formula
   end
 
   test do
-    assert_match "Optic Capture 0.2.0", shell_output("#{bin}/optic_capture --version")
+    assert_match "Optic Capture 0.2.1", shell_output("#{bin}/optic_capture --version")
   end
 end
